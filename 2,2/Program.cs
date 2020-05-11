b4ff459c322e7ecb396d84e7eea77f0b658fb8da
S7pt <chaos160702@gmail.com>
2020-05-11 17:51:51 +0300

    Added Files 2

---
+++ 2,2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2_2
+{
+    class Program
+    {
+        static void FunctionSolver()
+        {
+            double a = -6;
+            double y;
+         
+            while (a < 3)
+            {
+                y = Math.Pow(a, 4) - 12 * a + Math.Exp(a);
+                Console.WriteLine("\tx=" + a + "\ty=" + y);
+                a += 0.75;
+            }
+        }
+        static void Main(string[] args)
+        {
+            FunctionSolver(); 
+
+        }
+    }
+}
